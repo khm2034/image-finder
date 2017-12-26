1055832b3b6c27785a6f5babe4319ffd2492046c
Hyeonmin Ko <khm2034@gmail.com>
2017-12-27 01:48:13 +0900

    first commit

---
+++ image-finder/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace image_finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.preButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.ErrorImage = global::image_finder.Properties.Resources.no_image;
+            this.pictureBox1.Image = global::image_finder.Properties.Resources.no_image;
+            this.pictureBox1.ImageLocation = "center";
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Size = new System.Drawing.Size(303, 294);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("야놀자 야체 R", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(334, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("야놀자 야체 B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.searchButton.Location = new System.Drawing.Point(336, 116);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(125, 55);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("야놀자 야체 B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox1.Location = new System.Drawing.Point(343, 307);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Always On Top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("야놀자 야체 B", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Image-Finder";
+            // 
+            // preButton
+            // 
+            this.preButton.Location = new System.Drawing.Point(336, 201);
+            this.preButton.Name = "preButton";
+            this.preButton.Size = new System.Drawing.Size(51, 47);
+            this.preButton.TabIndex = 5;
+            this.preButton.Text = "Pre";
+            this.preButton.UseVisualStyleBackColor = true;
+            this.preButton.Click += new System.EventHandler(this.preButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(410, 201);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(51, 47);
+            this.nextButton.TabIndex = 6;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 385);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.preButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button preButton;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ image-finder/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Net;
+using System.IO;
+
+namespace image_finder
+{
+    public partial class Form1 : Form
+    {
+        string searchText = null;
+        string searchID = "009971028505315732222:bk2l2qliuws";
+        string key = "AIzaSyAGmbxC2vaACyOA_qZq5Yzt2blPrU-KrOo";
+        //ArrayList imageArr = new ArrayList(Image);
+        List<Image> imageArr = new List<Image>();
+        //Image[] imageArr;
+        int imageArrIndex;
+        int imageArrSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            searchText = t.Text;
+            Console.WriteLine(searchText);
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            imageArr.Clear();
+            imageArrIndex = 0;
+            imageArrSize = 0;
+            if (!searchText.Equals(""))
+            {
+                var svc = new Google.Apis.Customsearch.v1.CustomsearchService(new Google.Apis.Services.BaseClientService.Initializer {ApiKey = key });
+                Console.WriteLine(searchText);
+                //var listRequest = svc.Cse.List(searchText);
+                Google.Apis.Customsearch.v1.CseResource.ListRequest listRequest = svc.Cse.List(searchText);
+                listRequest.Cx = searchID;
+                listRequest.SearchType = Google.Apis.Customsearch.v1.CseResource.ListRequest.SearchTypeEnum.Image;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    listRequest.Start = 1 + 10*i;
+                    Google.Apis.Customsearch.v1.Data.Search search = listRequest.Execute();
+                    foreach (var result in search.Items)
+                    {
+                        imageArrIndex++;
+                        imageArr.Add((Image)BitmapFromURL(result.Image.ThumbnailLink));
+                       
+                        //if (!result.Pagemap.Equals(null) && result.Pagemap.ContainsKey("cse_thumbnail"))
+                        //{
+                        //    //Console.WriteLine(result.Pagemap["cse_thumbnail"][0]["src"]);
+                        //    imageArrIndex++;
+                        //    imageArr.Add((Image)BitmapFromURL(result.Pagemap["cse_thumbnail"][0]["src"].ToString()));
+                        //}
+                        //imageArr[imageArrIndex++] = result.Image.;
+                        //pictureBox1.Image = result.Image.ThumbnailLink;
+                    }
+                }
+                imageArrSize = imageArrIndex;
+                imageArrIndex = 0;
+                
+                pictureBox1.Image = imageArr[imageArrIndex];
+            }
+        }
+
+        public Bitmap BitmapFromURL(string URL)
+        {
+            try
+            {
+                WebClient Downloader = new WebClient();
+                Stream ImageStream = Downloader.OpenRead(URL);
+                Bitmap DownloadImage = Bitmap.FromStream(ImageStream) as Bitmap;
+                return DownloadImage;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private void preButton_Click(object sender, EventArgs e)
+        {
+            if(imageArrIndex != 0)
+            {
+                imageArrIndex--;
+                pictureBox1.Image = imageArr[imageArrIndex];
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (imageArrIndex != imageArrSize-1)
+            {
+                imageArrIndex++;
+                pictureBox1.Image = imageArr[imageArrIndex];
+            }
+        }
+    }
+}
